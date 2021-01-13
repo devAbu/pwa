eb3d40455b9c78c44a4408ce355c69bac210e1aa
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2021-01-13 21:04:46 +0100

    Promise and Promise all

--- project2/app.js
+++ project2/app.js
@@ -1,20 +1,39 @@
 const posts = [{ title: "post1" }, { title: "post2" }];
 
-function createPost(post, cb) {
-  const time = [1000, 3000, 5000];
-  const random = Math.floor(Math.random() * time.length);
+function createPost(post) {
+  return new Promise((resolve, rejects) => {
+    const time = [1000, 3000, 5000];
+    const random = Math.floor(Math.random() * time.length);
 
-  setTimeout(() => {
-    posts.push(post);
-    console.log(posts);
-    cb();
-  }, time[random]);
+    setTimeout(() => {
+      let error = false;
+      if (!error) {
+        posts.push(post);
+        resolve(posts);
+      } else {
+        rejects("Error in pushing posts");
+      }
+    }, time[random]);
+  });
 }
 
-createPost({ title: "post3" }, () => {
-  createPost({ title: "post4" }, () => {
-    createPost({ title: "post5" }, () => {
-      console.log("Finish");
-    });
+createPost({ title: "post3" })
+  .then((data) => createPost({ title: "post4" }))
+  .then((data) => createPost({ title: "post5" }))
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log(posts);
+    console.log("settled");
   });
-});
+
+// Promise all
+
+Promise.all([createPost({ title: "post6" }), createPost({ title: "post7" })])
+  .then(() => {
+    console.log("all posts", posts);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
